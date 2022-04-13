82dcd8cdda1979fdf02d33e11e033253b67faab5
Mon MacBook <justine.fernandez1990@gmail.com>
2022-04-13 17:05:35 +0200

    auth

--- index.js
+++ index.js
@@ -1,54 +1,98 @@
 const express = require("express");
 const app = express();
 const mysql = require("mysql");
+// const env = require("./.env")
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "password",
-  database: "employeeSystem",
+  user: "bfe061570069c2",
+  host: "eu-cdbr-west-02.cleardb.net",
+  password: "7705de63",
+  database: "heroku_c88c9286219a8f8",
 });
 
-app.get("/hotels", (req, res) => {
-  const hotels = [
-    {
-        hotelTitle:"La Bergerie",
-        roomsTitle:[
-            "La diamant",
-            "La rubis",
-            "La saphir",
-            "L'améthyste",
-            "L'eben"
-        ]
-    },
-    {
-        hotelTitle:"Le Chabichou",
-        roomsTitle:[
-            "La diamant1",
-            "La rubis1",
-            "La saphir1",
-            "L'améthyste1",
-            "L'eben1"
-        ]
-    },
-    {
-        hotelTitle:"L'Altapure'",
-        roomsTitle:[
-            "La diamant2",
-            "La rubis2",
-            "La saphir2",
-            "L'améthyste2",
-            "L'eben2"
-        ]
-    },
-]
-  res.send(hotels);
+app.post('/register', (req, res) => {
+
+  const name = req.body.name
+  const surname = req.body.surname
+  const mail = req.body.mail
+  const password = req.body.password
+  
+  db.query(
+    "INSERT INTO users (name, surname, mail, password) VALUES (?,?,?,?)",
+    [name, surname, mail, password],
+    (err, result)=>{
+      console.log(err);
+    }
+  );
 });
 
-app.listen(process.env.PORT || 3001, () => {
+app.post('/login', (req,res) => {
+
+  const mail = req.body.mail
+  const password = req.body.password
+  
+  db.query(
+    "SELECT * FROM users WHERE mail = ? AND password = ?",
+    [mail, password],
+    (err, result)=>{
+
+      if(err){
+        res.send({err:err});
+      }  
+        if (result.length>0){
+          res.send(result)
+        }else{
+            res.send({message:"Mauvaise combinaison utilisateur/mdp"})
+        }
+    
+  }
+  );
+
+})
+
+
+// app.get("/hotels", (req, res) => {
+//   const hotels = [
+//     {
+//         hotelTitle:"La Bergerie",
+//         roomsTitle:[
+//             "La diamant",
+//             "La rubis",
+//             "La saphir",
+//             "L'améthyste",
+//             "L'eben"
+//         ]
+//     },
+//     {
+//         hotelTitle:"Le Chabichou",
+//         roomsTitle:[
+//             "La diamant1",
+//             "La rubis1",
+//             "La saphir1",
+//             "L'améthyste1",
+//             "L'eben1"
+//         ]
+//     },
+//     {
+//         hotelTitle:"L'Altapure'",
+//         roomsTitle:[
+//             "La diamant2",
+//             "La rubis2",
+//             "La saphir2",
+//             "L'améthyste2",
+//             "L'eben2"
+//         ]
+//     },
+// ] 
+//   res.send(hotels);
+// });
+
+
+
+app.listen(process.env.PORT || 3000, () => {
   console.log("Yeah! Your backend server is running on port 3000");
 });
