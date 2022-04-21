8974a7c05864e12f3b0811bd1c8fc4a462888ee2
Mon MacBook <justine.fernandez1990@gmail.com>
2022-04-21 22:37:06 +0200

    fix bug cors

--- index.js
+++ index.js
@@ -6,7 +6,22 @@ require("dotenv").config();
 
 app.use(express.json());
 
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://hypnos-app.herokuapp.com');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(cors({
     origin: ['https://hypnos-app.herokuapp.com', 'http://localhost:3000']
 }));
