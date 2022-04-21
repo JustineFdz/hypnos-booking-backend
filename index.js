9184fb8f87b391a207d27c665114d0d43c6448c3
Mon MacBook <justine.fernandez1990@gmail.com>
2022-04-21 23:17:10 +0200

    index.js

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use(express.json());
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://hypnos-app.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
